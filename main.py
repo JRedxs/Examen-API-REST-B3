6d55ee4b546946f96736e9dc69ace84b2536301b
JRedxs <98217327+JRedxs@users.noreply.github.com>
2023-01-17 16:02:50 +0100

    ajout des fonctions sur le swagger

--- main.py
+++ main.py
@@ -6,3 +6,35 @@ import sqlite3
 from sqlite3 import Error
 
 app = FastAPI()
+
+
+class Article(BaseModel):
+    id: int
+    name: str
+    description: str
+    quantity: int
+
+class Commande(BaseModel):
+    id: int
+    listArticle: utils.List[Article]
+    status: str
+
+
+@app.post("/articles")
+async def Articles(Articles: Article):
+    pass
+
+
+@app.get("/articles/{articleId}")
+async def getArticles(articleId: int):
+    pass
+
+@app.put("/articles")
+async def putArticles(Articles: Article):
+    pass
+
+
+
+@app.post("/commands")
+async def postCommand(Commandes: Commande):
+    pass
