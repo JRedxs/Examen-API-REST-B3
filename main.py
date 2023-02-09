aafb9c1d615eec464e7812177586f7e05b249dcc
JRedxs <98217327+JRedxs@users.noreply.github.com>
2023-02-09 16:58:55 +0100

    ajout du get get by id et put

--- main.py
+++ main.py
@@ -5,42 +5,62 @@ from pydantic import BaseModel, utils
 import sqlite3
 from sqlite3 import Error
 
-db = sqlite3.connect("services.db")
+db = sqlite3.connect("services.db", check_same_thread=False)
 
 cursor = db.cursor()
 
 app = FastAPI()
 
 
-class Article(BaseModel):
-    id: int
+class ArticleTest(BaseModel):
+    id_article: int
     name: str
     description: str
     quantity: int
 
 class Commande(BaseModel):
     id: int
-    listArticle: utils.List[Article]
+    listArticle: utils.List[ArticleTest]
     status: str
 
 
-@app.post("/articles")
-async def Articles(Articles: Article):
-    db.execute("INSERT INTO Articles(?,?,?,?)", (Article.id,Article.name,Article.description,Article.quantity))
-    db.commit()
+@app.post("/article")
+async def AddArticles(Articles: ArticleTest):
+    if Articles.id_article not in Articles:
+        db.execute("INSERT into Article VALUES(?,?,?,?)", (Articles.id_article,
+                                                            Articles.name, Articles.description, Articles.quantity,))
+        db.commit()
+    else:
+        return{"ID deja existant"}
     return Articles
-
+    
 
 @app.get("/articles/{articleId}")
 async def getArticles(articleId: int):
-    pass
+    cursor.execute("SELECT * FROM Article WHERE id=?", (articleId,))
+    article = cursor.fetchone()
+
+    if article:
+        return {"article": article}
+    else:
+        return {"error": "Article not found"}
 
 @app.put("/articles")
-async def putArticles(Articles: Article):
-    pass
+async def putArticles(Articles: ArticleTest):
+    db.execute("UPDATE Article SET name=?, description=?, quantity=? WHERE id = ?", (Articles.name,Articles.description,Articles.quantity,Articles.id_article))
+    db.commit()
 
+    return Articles
 
 
 @app.post("/commands")
 async def postCommand(Commandes: Commande):
     pass
+
+@app.get("/articles")
+def get_articles():
+            cursor.execute("SELECT * FROM Article")
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+            return {"Article" : rows}
